108e232ad84e6bfd8ded312cc6e8edf71ddf1da2
Kamol Roy <kamol.roy9943@gmail.com>
2018-11-23 13:42:06 +0600

    header and main-menubar desing and resolve the css issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderTopComponent } from './header-top/header-top.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderTopComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule
